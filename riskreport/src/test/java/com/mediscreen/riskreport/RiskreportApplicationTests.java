31838082990a24717a89c1d70ae5b4ed1846a910
Clément Guio <clementguio@gmail.com>
2022-12-19 12:30:46 +0100

    Create RiskReport microservice with first draft of model and service

---
+++ riskreport/src/test/java/com/mediscreen/riskreport/RiskreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.riskreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
